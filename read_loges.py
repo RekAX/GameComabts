2da6396ef09473996951a9aefcab57528f1b3311
RekAX <ilkin.zeynalovich@gmail.com>
2018-03-17 22:07:52 +0400

    Update read_loges.py

--- read_loges.py
+++ read_loges.py
@@ -7,58 +7,53 @@ import shutil
 from shutil import copyfile
 from removepic import del_pic
 
-
+'''Number terminal ,date and Account number!'''
 con = input('Cashin nomre: ')
-daxil = int(input("Odenish edilen gun: "))
+daxil_gun = int(input("Odenish edilen gun: "))
 ay = input("odenish edilen ay: ")
 il = input("odenish edilen il: ")
 a= 1
 
+'''Delete all pictures in locale directories'''
 pic_dir = 'C:/logs/image'
 for file in os.listdir(pic_dir):
     if file.endswith('.png'):
         os.remove(file)
 
-
+'''Create local list'''
 cedvel = []
 
 
-if daxil < 9:
-    gun ='0{}'.format(daxil + a)
+if daxil_gun < 9:  # if day is tiny 9
+    gun ='0{}'.format(daxil_gun + a)
 else:
-    gun = daxil + a
+    gun = daxil_gun + a
 
 
 def copy ():
     '''With threads copy currently file. if code give any error day + 1 we can as see'''
     try:
-        print(threading.current_thread().getName(), 'is Starting')
         copy = copyfile('//cash' + con + '/CashIn/log/CashInTerminalWpf_debug_{}-{}-{}_00000.txt'.format(gun, ay, il),
                         'C:/logs/terminal.txt')
+        print(threading.current_thread().getName(), 'is Exiting')  #For notification exitinf copy
 
-        print(threading.current_thread().getName(), 'is Exiting')
 
     except FileNotFoundError:
 
-        print(threading.current_thread().getName(), 'is Starting')
 
         copy = copyfile('//cash' + con + '/CashIn/log/CashInTerminalWpf_debug.txt',
                         'C:/logs/terminal.txt')
-        print(threading.current_thread().getName(), 'is Exiting')
-
-
+        print(threading.current_thread().getName(), 'is Exiting') #For notification exitinf copy
 
 def bar():
-    #print(threading.current_thread().getName(), 'is Starting')
-    for i in tqdm(range(100)):
+    for i in tqdm(range(100)):   # progress bar
 
         time.sleep(0.05)
     time.sleep(0.01)
     emeliyyat()
-    #print(threading.current_thread().getName(), 'is Exiting')
 
 
-
+"""Start treading bar and copy"""
 tre1 = threading.Thread(target=bar,name='bar')
 tre2 = threading.Thread(target=copy,name='copy')
 
@@ -68,19 +63,14 @@ tre2.start()
 
 
 
-
+'''Copy All picture when payment is stacked'''
 def pic(con,il,ay,gun,saat,deq,san):
-    global daxil
 
-
-    #os.remove('C:/logs/image/*.jpg')
-    #print(con)
-    if daxil < 9:
-        gun = '0{}'.format(daxil)
+    if daxil_gun < 9:
+        gun = '0{}'.format(daxil_gun)
     else:
-        gun = daxil
+        gun = daxil_gun
 
-    #print('def - san',san)
 
     if int(san) < 4:
         deq = int(deq) - 1
@@ -98,47 +88,42 @@ def pic(con,il,ay,gun,saat,deq,san):
 
 
 
-    #print('san = ',san,'deq',deq,'saat= ',saat)
-    line = [int(san) - 1, int(san), int(san) + 1]
+    line = [int(san) - 1, int(san), int(san) + 1] #add list second when stacked and +1 second -1 second
 
-    #print('line',line)
 
     for a in line:
-        if a < 10:
+        if a < 10:    # if second tiny 10 write 0 before second
             a = '0{}'.format(a)
             print('oldu = ',a)
 
-        if a == 60:
+        if a == 60:   # if second is 60 append 1 minute and second equal 00
             a = '00'
             deq = deq + 1
             if deq == 60:
                 saat = saat + 1
-        mydir = '//cash' + con + '/images/{}{}{}/'.format(il, ay, gun)
-        #print('line a:',a)
-        #print('deq = ',deq)
+        mydir = '//cash' + con + '/images/{}{}{}/'.format(il, ay, gun)   # add temrinal directory when we can copy pictures
 
 
-        for file in os.listdir(mydir) :
 
-            if file.startswith('{}{}{}_'.format(saat,deq,a)) and file.endswith('_{}_bottom.jpg'.format(con)):
+        for file in os.listdir(mydir) : # append to cycle all files in directory
+
+            if file.startswith('{}{}{}_'.format(saat,deq,a)) and file.endswith('_{}_bottom.jpg'.format(con)): #all files when startend stacked date and ended .jpg
                 print('copyfile{}{}{}'.format(saat, deq, a))
                 time.sleep(0.001)
             #print(mydir+file)
                 copydir = mydir+file
-                shutil.copy2(copydir, 'C:/logs/image/terminalloadpic{}{}{}.jpg'.format(saat, deq, a))
+                shutil.copy2(copydir, 'C:/logs/image/terminalloadpic{}{}{}.jpg'.format(saat, deq, a)) #copy all pictures local directory
           #      print(copydir, 'C:/logs/image/terminalloadpic{}{}{}.jpg'.format(saat, deq, a))
 
 
-
+'''All process '''
 def emeliyyat():
-    del_pic()
-    global cedvel
+    del_pic() # delete all old pictures
     cek = 'CashInTerminalWpf.PagePaySuccess.PrintDocumentOnPrintPage -'
     sonmek = 'CashInTerminalWpf.MainWindow.DoClose - LogFilesCheckerThread'
     ilishme = 'CashInTerminalWpf.PageMoneyInput.CcnetDeviceOnReadCommand - State: JamInAcceptor'
     transID = 'CashInTerminalWpf.PageMoneyInput.InitLocalDbAndInsertPaymentValues - Starting transId:'
     Input_Text = 'CashInTerminalWpf.PageMoneyInput.CcnetDeviceOnBillStacked - Stacked'
-   # hesab = 'Hesab nömrəsi: '+ input("search :")
     pasport = "Pasport №:"
     Code = input("Search personal code ==> :")
     hesab = 'Hesab nömrəsi: ' + Code
@@ -149,35 +134,34 @@ def emeliyyat():
 
 
 
-    with open("C:/logs/terminal.txt",encoding='utf-8') as file:
+    with open("C:/logs/terminal.txt",encoding='utf-8') as file: #open log file and reading
 
-        lines = file.readlines()
-        for h in lines:
+        lines = file.readlines() # append reading file to list
+        for h in lines:  # search in list key sentences
             #if hesab in h or abonent in h or accoount in h:
             if hesab in h or abonent in h or accoount in h or pul_kocurme in h:
 
                 print(h)
                 for w in lines[counter:]:
-                    if transID in w:
+                    if transID in w:                                #find tranjaction id
                         print(w[:14],w[96:127])
-                    if Input_Text in w:
-                        cedvel.append(w[6:14])
+                    if Input_Text in w:                             # find all stacked pay
+                        cedvel.append(w[6:14])                      #add all stacked pay
                         print(w[:14],w[85:].replace('\n', ''))
-                    if cek in w:
+                    if cek in w:                                    #print chek
                         print(w[85:].replace('\n', ''))
-                    if sonmek in w :
+                    if sonmek in w :                                # find shuthdown error code
                         print('Texniki nasazlıq - Terminal Sönüb')
                         exit()
-                    if ilishme in w:
+                    if ilishme in w:                                # find when pay is jammed
                         print(w[:14].replace('\n', ''),'- Caşında  ilişmə olub!!!')
-                    if pasport in w:
+                    if pasport in w:                                # end person session when created new pasport number
                         break
 
 
 
             counter=counter+1
-    #global daxil
-    for a in cedvel:
+    for a in cedvel:                
 
         saat = a[:2]
         deq = a[3:5]
